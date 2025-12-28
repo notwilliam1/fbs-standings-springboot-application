e8b381b66c5340d459c42433ff77d1cb0a63e222
William Causey <causeywilliam0@gmail.com>
2025-12-28 15:22:38 -0500

    Added cross origin to backend and created start of react app in app.js

--- backend/src/main/java/com/ncaafbs/fbs_standings/team/TeamController.java
+++ backend/src/main/java/com/ncaafbs/fbs_standings/team/TeamController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/v1/team")
+@RequestMapping(path = "/api/v1/team")
+@CrossOrigin(origins = "http://localhost:3000")
 public class TeamController {
     private final TeamService teamService;
 
--- frontend/fbs-standings-v1/src/App.js
+++ frontend/fbs-standings-v1/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import api from './api/axiosConfig';
+import { useState, useEffect, use } from 'react';
 
 function App() {
+
+  const [teams, setTeams] = useState([]);
+
+  const getTeams = async () => {
+    try {
+      const response =  await api.get("/api/v1/team");
+
+      console.log("Response data:", response.data);
+
+      setTeams(response.data);
+
+    } catch (error) {
+      console.log("Error fetching teams:", error);
+    }
+
+  }
+
+  useEffect(() => {
+    getTeams();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
