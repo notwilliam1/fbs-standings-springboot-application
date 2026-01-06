77c5a07027fecd82df77bf29fe4acbcffc9a70e2
William Causey <causeywilliam0@gmail.com>
2026-01-06 01:35:49 -0500

    Created Folder and added Home layout
    Created home folder in components folder to create the home layout for react. Updated App.js to include the Route path to home.

--- frontend/fbs-standings-v1/src/App.js
+++ frontend/fbs-standings-v1/src/App.js
@@ -3,6 +3,7 @@ import api from './api/axiosConfig';
 import { useState, useEffect, use } from 'react';
 import Layout from './components/Layout';
 import {Routes, Route} from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
 
@@ -28,8 +29,8 @@ function App() {
     <div className="App">
       
       <Routes>
-        <Route path="/" element={Layout}>
-          
+        <Route path="/" element={<Layout/>}>
+          <Route path="/" element={<Home />} ></Route>
 
         </Route>
       </Routes>
---
+++ frontend/fbs-standings-v1/src/components/home/Home.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Home = () => {
+  return (
+    <div>
+      Welcome to the FBS Standings Application!
+    </div>
+  )
+}
+
+export default Home
